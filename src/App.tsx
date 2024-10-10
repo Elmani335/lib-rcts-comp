3ad184409fb414359acc60b74007092222b24c6a
Florian <113524385+Florianrcht@users.noreply.github.com>
2024-10-10 10:48:32 +0200

    Select pre

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import RctsComptBtn from "./components/button/button";
 import "./App.css";
+import RctsComptSelect from "./components/select/select";
 
 const App: React.FC = () => {
   return (
@@ -10,6 +11,21 @@ const App: React.FC = () => {
           {index === 0 && (
             <RctsComptBtn text="Click Me!" primary size="medium" />
           )}
+          {index === 1 && (
+            <RctsComptSelect
+                id="exampleSelect"
+                name="example"
+                data={[
+                    { group: "Renault", options: [{ value: "clio2", label: "Clio 2" }, { value: "Megane3", label: "Megane 3" }] },
+                    { group: "Peugeot", options: [{ value: "206", label: "206" }, { value: "5008", label: "5008" }] },
+                    { value: "Autre", label: "autre" } // Option simple
+                ]}
+                withLabel={true}
+                text="Select an option"
+                size="medium"
+                onChange={(e) => console.log(e.target.value)}
+            />
+          )}        
         </div>
       ))}
     </div>
