5060099049b88e281497285242b402bb7c99953a
Florian <113524385+Florianrcht@users.noreply.github.com>
2024-10-11 09:39:17 +0200

    checkbox move

--- src/App.tsx
+++ src/App.tsx
@@ -84,6 +84,33 @@ const App: React.FC = () => {
               )}
             </div>
           )}   
+            {index === 4 && (
+            /* Personnalisation des props */
+            <div style={{ padding: "20px" }}>
+                <Checkbox
+                name="exampleCheckbox"
+                text="La Centrale"
+                data={[
+                    { group: "Moteur", options: [{ value: "1.4", label: "1.4" }, { value: "1.2", label: "1.2" }, { value: "1.9", label: "1.9" }] },
+                    { group: "Generation", options: [{ value: "RS", label: "RS" }, { value: "Coupe", label: "Coupe" }, { value: "Societe", label: "Societe" }] },
+                    { group: "Carburant", options: [{ value: "Essence", label: "Essence" }, { value: "Diesel", label: "Diesel" }, { value: "Electrique", label: "Electrique" }] },
+                    { value: "1erMain", label: "1er Main" } 
+                ]}
+                onChange={(e) => {
+                    const selectedValue = e.target.value;
+                    console.log(selectedValue);
+                }}
+                value={["1.4", "Essence"]} 
+                disabled={false}
+                required={true}
+                display="vertical"
+                withLabel={true}
+                fontSize="fontSizeMedium"
+                border
+                size="large"
+                />
+            </div>
+          )}
           {index === 10 && (
             /* Personnalisation des props */
             <div>
@@ -124,33 +151,6 @@ const App: React.FC = () => {
           {index === 14 && (
             <div style={{ padding: "20px" }}>
               <Radio options={["Voiture", "Bateau", "Avion"]} name="radio" onChange={handleRadioChange} />
-            </div>
-          )}
-          {index === 11 && (
-            /* Personnalisation des props */
-            <div style={{ padding: "20px" }}>
-                <Checkbox
-                name="exampleCheckbox"
-                text="La Centrale"
-                data={[
-                    { group: "Moteur", options: [{ value: "1.4", label: "1.4" }, { value: "1.2", label: "1.2" }, { value: "1.9", label: "1.9" }] },
-                    { group: "Generation", options: [{ value: "RS", label: "RS" }, { value: "Coupe", label: "Coupe" }, { value: "Societe", label: "Societe" }] },
-                    { group: "Carburant", options: [{ value: "Essence", label: "Essence" }, { value: "Diesel", label: "Diesel" }, { value: "Electrique", label: "Electrique" }] },
-                    { value: "1erMain", label: "1er Main" } 
-                ]}
-                onChange={(e) => {
-                    const selectedValue = e.target.value;
-                    console.log(selectedValue);
-                }}
-                value={["1.4", "Essence"]} 
-                disabled={false}
-                required={true}
-                display="vertical"
-                withLabel={true}
-                fontSize="fontSizeMedium"
-                border
-                size="large"
-                />
             </div>
           )}
         </div>
